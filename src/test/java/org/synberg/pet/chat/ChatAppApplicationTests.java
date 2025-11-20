087165242794dc31f027f76cf8abba42d6141614
Synberg <synberg@yandex.ru>
2025-11-20 13:58:10 +0400

    Initial commit

---
+++ src/test/java/org/synberg/pet/chat/ChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.synberg.pet.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

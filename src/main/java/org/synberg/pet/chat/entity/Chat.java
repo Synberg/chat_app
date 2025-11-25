2038499665143e225e9b4f647cf418bb7b53a140
Synberg <synberg@yandex.ru>
2025-11-25 15:19:06 +0400

    fix: add missing JoinColumn for Chat.user1 and Chat.user2

--- src/main/java/org/synberg/pet/chat/entity/Chat.java
+++ src/main/java/org/synberg/pet/chat/entity/Chat.java
@@ -14,8 +14,10 @@ public class Chat {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user1_id", nullable = false)
     private User user1;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user2_id", nullable = false)
     private User user2;
 }
